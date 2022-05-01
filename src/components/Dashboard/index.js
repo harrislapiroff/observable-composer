e2e26e4d91418815e305e38f62e264472b33e9de
Harris Lapiroff <harris@freedom.press>
2022-05-01 18:46:21 -0400

    Move cell rendering logic to Editor and show cell list

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,49 +1,20 @@
-import { Runtime, Inspector } from '@observablehq/runtime'
-import { useEffect, useRef } from 'react'
+import ErrorBoundary from '../ErrorBoundary.js'
 
 import Cell from './Cell.js'
 
-export default function Dashboard({ config }) {
-    useEffect(() => {
-        (async () => {
-            const notebookUrl = `https://api.observablehq.com/${config.notebook}.js?v=3`
-            const { default: notebook } = await import(/* webpackIgnore: true */notebookUrl)
-            new Runtime().module(notebook, name => {
-                if (config.cells.map(d => d.name).includes(name)) {
-                    return new Inspector(cellRefs.current.get(name))
-                }
-            })
-        })()
-    }, [config])
-
-    const cellRefs = useRef(new Map())
-
+export default function Dashboard({ config, children }) {
     return (
-        <div style={{
-            display: 'grid',
-            columnGap: config.grid.columnGap,
-            rowGap: config.grid.rowGap,
-        }}>
-            {config.cells.map(
-                ({
-                    name,
-                    height,
-                    width,
-                    column,
-                    columnSpan,
-                    row,
-                    rowSpan,
-                }) => <Cell
-                    height={height}
-                    width={width}
-                    column={column}
-                    columnSpan={columnSpan}
-                    row={row}
-                    rowSpan={rowSpan}
-                    key={name}
-                    ref={el => cellRefs.current.set(name, el)}
-                />
-            )}
+        <div
+            className="bg-white p-3"
+            style={{
+                display: 'grid',
+                columnGap: config.grid.columnGap,
+                rowGap: config.grid.rowGap,
+            }}
+        >
+            <ErrorBoundary>
+                {children}
+            </ErrorBoundary>
         </div>
     )
 }
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,9 +1,11 @@
-import { Runtime, Inspector } from '@observablehq/runtime'
-import { useState, useEffect } from "react"
+import { Runtime, Inspector, Library } from '@observablehq/runtime'
+import { useState, useEffect, useRef } from "react"
 import Dashboard from "../Dashboard"
+import Cell from '../Dashboard/Cell'
 
 export default function Editor({ config }) {
     const [module, setModule] = useState()
+    const cellDivs = useRef(new Map())
 
     useEffect(() => {
         let dispose = () => {}
@@ -11,22 +13,68 @@ export default function Editor({ config }) {
             const notebookUrl = `https://api.observablehq.com/${config.notebook}.js?v=3`
             const { default: notebook } = await import(/* webpackIgnore: true */notebookUrl)
             const runtime = new Runtime()
-            const module = runtime.module(notebook)
+            const module = runtime.module(notebook, name => {
+                if (cellDivs.current.has(name)) {
+                    return new Inspector(cellDivs.current.get(name))
+                }
+            })
             setModule(module)
             dispose = d => runtime.dispose()
         })()
         return () => dispose()
     }, [config])
 
+    const nonCellVars = module ? Array.from(module._scope).filter(([name, _]) => !module._runtime._builtin._scope.has(name)) : []
+    
     return (
         <div className="flex h-screen">
-            <div className="bg-slate-50 border-r border-slate-300 p-5 w-64 shrink-0">
+            <div className="border-r border-slate-300 p-5 w-64 shrink-0 overflow-auto">
                 <h2 className="font-semibold">{config.notebook}</h2>
+                {nonCellVars.map(([name, def]) => <button
+                    className={[
+                        'bg-slate-100',
+                        'px-2',
+                        'py-1',
+                        'border-radius-1',
+                        'my-2',
+                        'rounded-md',
+                        'block',
+                        'w-full',
+                        'text-left',
+                        'hover:bg-slate-200',
+                        'text-slate-800',
+                        'text-sm',
+                        cellDivs.current.has(name) ? 'border' : null,
+                        'border-slate-400'
+                    ].join(' ')}
+                    key={name
+                }>{name}</button>)}
             </div>
-            <div className="p-5">
-                <Dashboard config={config} />
+            <div className="p-5 bg-slate-50 overflow-auto">
+                <Dashboard config={config}>
+                    {config.cells.map(({
+                        name,
+                        height,
+                        width,
+                        column,
+                        columnSpan,
+                        row,
+                        rowSpan,
+                    }) => {
+                        return <Cell
+                            height={height}
+                            width={width}
+                            column={column}
+                            columnSpan={columnSpan}
+                            row={row}
+                            rowSpan={rowSpan}
+                            key={name}
+                            ref={d => cellDivs.current.set(name, d)}
+                        />
+                    })}
+                </Dashboard>
             </div>
-            <div className="bg-slate-50 border-slate-300 p-5 border-l w-64 shrink-0">
+            <div className="border-slate-300 p-5 border-l w-64 shrink-0 overflow-auto">
                 <h2 className="font-semibold">Grid</h2>
                 Columns: {config.grid.columns || 'auto'}<br />
                 Rows: {config.grid.rows || 'auto'}<br />
